e02f5cf7cee4591481b585882e68c4ae3d7f90f5
TN.RIJ4L <59259192+rijal-cpu@users.noreply.github.com>
2025-01-08 12:44:55 +0800

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -3,7 +3,7 @@
 # Skrip ini digunakan untuk memeriksa apakah sistem rentan terhadap beberapa kerentanan utama dari tahun 2021 hingga 2024
 # Termasuk CVE-2021-4034, CVE-2022-0847 (Dirty Pipe), CVE-2023-3215, dan CVE-2024-0013
 # Dan menambahkan pemeriksaan layanan seperti MYSQL, PERL, PYTHON, WGET, CURL, GCC, dan PKEXEC
-# kalau eror gunakan perintah ini sed -i 's/\r//' d.sh
+# kalau eror gunakan perintah ini sed -i 's/\r//' ok.sh
 
 echo "===== Scanner Kerentanan dan Status Layanan ====="
 
