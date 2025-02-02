2517fd7a5c3576a957908f64b4237f7c452d9cb8
TN.RIJ4L <59259192+rijal-cpu@users.noreply.github.com>
2025-02-02 17:15:27 +0800

    Update bersih.sh

--- bersih.sh
+++ bersih.sh
@@ -4,9 +4,9 @@ rm -rf /root/.bash_history .bash_logout .bash_profile
 rm -rf /tmp/*
 chmod -R 000 /var/log/
 find /var/log/ -type f -exec truncate -s 0 {} \;
+find /var/www/vhosts/ -type f -name .bash_history -exec rm -f {} \;
 sudo yum clean all
 sudo yum makecache
-find /var/www/vhosts/ -type f -name .bash_history -exec rm -f {} \;
 service nginx restart
 /sbin/service sshd restart
 /sbin/init 6
